f4e2e927b9ae066481d057f787b4f025514a2060
Ben <papasofts@gmail.com>
2019-10-21 00:44:39 +0300

    updating homepage

--- frontpage/src/components/Home.vue
+++ frontpage/src/components/Home.vue
@@ -2,12 +2,11 @@
     <main role="main">
       <div class="jumbotron">
         <h1>Django-Vue-App</h1>
-        <p class="lead">This application uses Django and Django REST Framework for backend, Vue for the frontend and oaAuth2 for handling user's authentication</p>
-        <a class="btn btn-lg btn-primary" href="" role="button">View tutorial</a>
+        <p class="lead">This application uses Django and Django REST Framework for backend, Vue for the frontend and oAuth2 authentication</p>
       </div>
 
     <h4>
-      You are not logged in! Please <a class="btn btn-primary" @click="authenticate('google')">Log In</a> to continue.
+      <a class="btn btn-primary" @click="authenticate('google')">Log In</a>
     </h4>
     </main>
 </template>
